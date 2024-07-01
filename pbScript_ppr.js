1bbd2201a4dd0609ec1f47d4c77f244fc2f11141
gotDoubleDots <gotdoubledots@gmail.com>
2024-07-01 11:27:33 -0500

    Update pbScript_ppr.js

--- pbScript_ppr.js
+++ pbScript_ppr.js
@@ -7,7 +7,7 @@ const setpointDuration = .25;
 // 1, 2, 3 combo/perm of random, sequenced array style
 let globalIterator=0;
 
-const songPath = './ppr.mp3'; // maybe last unaware halluc typo was .mpg
+const songPath = './khhfr.mp3'; // maybe last unaware halluc typo was .mpg
 
 
 let volArr=[];
